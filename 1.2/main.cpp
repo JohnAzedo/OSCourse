e137e4796c17853ba1e32e7b20aa47e4b634deeb
JohnAzedo <jplimao12@gmail.com>
2021-02-23 00:27:27 -0300

    Finalizar função 1 com threads

--- 1.2/main.cpp
+++ 1.2/main.cpp
@@ -3,18 +3,15 @@
 #include <math.h>
 #include <string>
 #include <vector>
+#include <unistd.h>
 
 #define PI 3.14159265358979323846
 
 // Temp vars
-#define NTHREADS 6
-#define NTRAPEZOIDS 120
+#define NTHREADS 7
+#define NTRAPEZOIDS 200
 
 using namespace std;
-
-float area_trapezoid(float B, float b, float h){
-    return (h/2)*(B+b);
-}
 
 //x0 = 0 and x = 2pi
 float f2(float x){
@@ -26,22 +23,10 @@ float f1(float x){
     return 5.0;
 }
 
-float calc(int trapezoids_per_thread, float x, float x0){
-    float h, sum, B, b;
-    h = (x-x0)/(float)trapezoids_per_thread; sum = 0; B = x0; b = h;
-
-    for(int j=0; j<trapezoids_per_thread; j++){
-        sum += (h/2)*(f1(B)+f1(b));
-        b = B;
-        B += h;
-    }
-    return sum;
-}
-
 void split_in_box(int h, int num_threads, int * array){
     int index = 0;
     for(int i=0; i<h; i++){
-        if(index==6){
+        if(index==num_threads){
             index = 0;
         }
         array[index] += 1;
@@ -49,28 +34,75 @@ void split_in_box(int h, int num_threads, int * array){
     }
 }
 
+float f1_sum = 0;
+float f2_sum = 0;
+
+struct Arguments {
+    int id;
+    // Trapezoids per thread
+    int tpt;
+    float a;
+    float b;
+};
+
+
+void * f1_in_thread(void* arguments){
+    auto *args = (Arguments *) arguments;
+
+    float h, a, b;
+    h = (args->b - args->a)/(float) args->tpt;
+    a = args->a; b = h;
+
+    for(int i=0; i<args->tpt; i++){
+        f1_sum += (h/2)*(f1(a)+f1(b));
+        a = b;
+        b += h;
+    }
+
+    pthread_exit(nullptr);
+}
+
+
 int main() {
     float x, x0; x = 10; x0 = 0;
-    float sum = 0;
-    float a, b = 0;
 
     pthread_t threads[NTHREADS];
-    void* return_thread;
+    void* thread_return;
+    int status;
 
-    int trapezoids_per_thread;
     int trapezoids_per_unit = floor(NTRAPEZOIDS/(x-x0));
     vector<int> array(NTHREADS, 0);
     split_in_box(10, NTHREADS, array.data());
 
-    // TODO: Adapt this to pthreads
+    Arguments args{
+            0,0,0,0
+    };
+
+    vector<Arguments> args_array(NTHREADS);
     for(int i=0; i<NTHREADS; i++){
-        b += array[i];
-        trapezoids_per_thread = array[i]*trapezoids_per_unit;
-        sum += calc(trapezoids_per_thread, b, a);
-        a = b;
+        args.id = i;
+        args.tpt = array[i]*trapezoids_per_unit;
+        args.b += array[i];
+        args_array[i] = args;
+        args.a = args.b;
     }
 
-    fprintf(stdout, "Soma: %.2f", sum);
+    for(int i=0; i<NTHREADS; i++){
+        status = pthread_create(&threads[i], nullptr, f1_in_thread, (void*) &args_array[i]);
+
+        if(status!=0){
+            fprintf(stdout, "Error");
+            return status;
+        }
+
+    }
+
+    for(int i=0; i<NTHREADS; i++){
+        pthread_join(threads[i], &thread_return);
+        fprintf(stdout,"Finish thread %i\n", i);
+    }
+
+    fprintf(stdout, "Soma: %.2f", f1_sum);
 
     return 0;
 }
