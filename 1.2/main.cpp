109fc36b96380fa4f84323ee78ca0a46157fcced
JohnAzedo <jplimao12@gmail.com>
2021-02-22 22:11:01 -0300

    Adicionar lógica sem pthreads

--- 1.2/main.cpp
+++ 1.2/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include "pthread.h"
 #include <math.h>
+#include <string>
+#include <vector>
+
 #define PI 3.14159265358979323846
 
 // Temp vars
 #define NTHREADS 6
 #define NTRAPEZOIDS 120
+
+using namespace std;
 
 float area_trapezoid(float B, float b, float h){
     return (h/2)*(B+b);
@@ -33,18 +38,39 @@ float calc(int trapezoids_per_thread, float x, float x0){
     return sum;
 }
 
+void split_in_box(int h, int num_threads, int * array){
+    int index = 0;
+    for(int i=0; i<h; i++){
+        if(index==6){
+            index = 0;
+        }
+        array[index] += 1;
+        index++;
+    }
+}
+
 int main() {
+    float x, x0; x = 10; x0 = 0;
+    float sum = 0;
+    float a, b = 0;
+
     pthread_t threads[NTHREADS];
     void* return_thread;
 
-    int trapezoids_per_thread = floor(NTRAPEZOIDS/NTHREADS);
-    int x = ceil(10/6);
+    int trapezoids_per_thread;
+    int trapezoids_per_unit = floor(NTRAPEZOIDS/(x-x0));
+    vector<int> array(NTHREADS, 0);
+    split_in_box(10, NTHREADS, array.data());
 
-    fprintf(stdout, "Test: %i", x);
-
+    // TODO: Adapt this to pthreads
     for(int i=0; i<NTHREADS; i++){
-
+        b += array[i];
+        trapezoids_per_thread = array[i]*trapezoids_per_unit;
+        sum += calc(trapezoids_per_thread, b, a);
+        a = b;
     }
+
+    fprintf(stdout, "Soma: %.2f", sum);
 
     return 0;
 }
